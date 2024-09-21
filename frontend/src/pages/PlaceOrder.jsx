31e0a69b6ff950d19f6e49cb1d94e3a855a789cb
madenon <164950634+madenon@users.noreply.github.com>
2024-09-21 21:24:00 +0100

    Update PlaceOrder.jsx

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -5,6 +5,7 @@ import { assets } from "../assets/assets";
 import { ShopContext } from "../context/ShopContext";
 import axios from "axios";
 import { toast } from "react-toastify";
+import Login from "./Login";
 
 const PlaceOrder = () => {
   const { navigate, backendUrl, cartItems, token,getCartAmount , setCartItems, delivery_fee, products } = useContext(ShopContext);
@@ -86,167 +87,168 @@ switch(method){
  }
 
   }
-  return (
+  return (!token  ?  <Login /> :
     <form onSubmit={onSubmitHanlder}
-      className="flex flex-col sm:flex-row justify-between gap-4
-    pt-5 sm:pt-5 sm:min-h-14 min-h-[80vh] border-t"
-    >
-      {/* ----------------  Lef Side ---- */}
-      <div className="flex flex-col gap-4 w-full sm:max-w-[400px]">
-        <div className="text-xl sm:text-xl my-3">
-          <Title text1={"Infromation Sur"} text2={"La livraison"} />
-        </div>
+    className="flex flex-col sm:flex-row justify-between gap-4
+  pt-5 sm:pt-5 sm:min-h-14 min-h-[80vh] border-t"
+  >
+    {/* ----------------  Lef Side ---- */}
+    <div className="flex flex-col gap-4 w-full sm:max-w-[400px]">
+      <div className="text-xl sm:text-xl my-3">
+        <Title text1={"Infromation Sur"} text2={"La livraison"} />
+      </div>
 
-        <p className="text-center  text-slate-400 font-semibold   justify-center ">
-          Nom et prénom  <span className="w-1 text-red-500 text-2xl font-bold">*</span>
-        </p>
-        <div className="flex gap-3">
-          <input onChange={onChangeHandler} value={formData.firstName} name="firstName"
-            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
-            type="text"
-            placeholder="Votre nom"
-            required
-          />
-          <input
-          onChange={onChangeHandler} value={formData.lastName} name="lastName"
-            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
-            type="text"
-            placeholder="Votre prénom"
-            required
-          />
-        </div>
-        <p className="text-center  text-slate-400 font-semibold  justify-center">
-          Email <span className="w-1 text-red-500 text-2xl font-bold">*</span>
-        </p>
-        <input
-        onChange={onChangeHandler} value={formData.email} name="email"
-          type="email"
-          className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
-          placeholder="votre email"
-          required
-        />
-        <p className="text-center  text-slate-400 font-semibold  justify-center">
-          La rue <span className="w-1 text-red-500 text-2xl font-bold">*</span>
-        </p>
-        <input
-        onChange={onChangeHandler} name="rue" value={formData.rue}
-          type="text"
-          className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
-          placeholder="La rue"
-          required
-        />
-
-        <p className="text-center  text-slate-400 font-semibold   justify-center">
-          Ville et la commune <span className="w-1 text-red-500 text-2xl font-bold">*</span>
-        </p>
-        <div className="flex gap-3">
-          <input
-          onChange={onChangeHandler}  value={formData.city}  name="city"
-            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
-            type="text"
-            placeholder="Ville"
-            required
-          />
-          <input
-          onChange={onChangeHandler}  name="state" value={formData.state}
-            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
-            type="text"
-            placeholder="Commune"
-            required
-          />
-        </div>
-
-        <p className="text-center  text-slate-400 font-semibold  justify-center">
-          Code postal et pays <span className="w-1 text-red-500 text-2xl font-bold">*</span>
-        </p>
-        <div className="flex gap-3">
-          <input
-          onChange={onChangeHandler} value={formData.zipcode}  name="zipcode"
-            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
-            type="number"
-            placeholder="Code postal"
-            required
-          />
-          <input
-          onChange={onChangeHandler} name="country"  value={formData.country}
-            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
-            type="text"
-            placeholder="Pays"
-            required
-          />
-        </div>
-
-        <p className="text-center  text-slate-400 font-semibold  justify-center">
-          {" "}
-          Votre numéro de téléphone portable  <span className="w-1 text-red-500 text-2xl font-bold">*</span>
-        </p>
-        <input
-        onChange={onChangeHandler} value={formData.phone} name="phone"
+      <p className="text-center  text-slate-400 font-semibold   justify-center ">
+        Nom et prénom  <span className="w-1 text-red-500 text-2xl font-bold">*</span>
+      </p>
+      <div className="flex gap-3">
+        <input onChange={onChangeHandler} value={formData.firstName} name="firstName"
           className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
           type="text"
-          placeholder="Votre numéro"
+          placeholder="Votre nom"
           required
         />
-       
-       <p className="text-center  text-slate-400 font-semibold  justify-center">
-          {" "}
-          Vous pouvez recevoir le colis sur un poit rélais  
-        </p>
-       
         <input
-        onChange={onChangeHandler} value={formData.pointrelaix} name="pointrelaix"
+        onChange={onChangeHandler} value={formData.lastName} name="lastName"
           className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
           type="text"
-          placeholder="Ecole, lieu plubic, boutique .. "
+          placeholder="Votre prénom"
+          required
         />
       </div>
-      {/* 88888888888888 cotde droit ---- */}
-      <div className="mt-8">
-        <div className="mt-8 min-w-80">
-          <CartTotal />
-        </div>
-        <div className="mt-12">
-          <Title text1={"Methode de "} text2={"Payyement"} />
-          {/*Methode de  Pyament */}
-          <div className="flex gap-3 flex-col lg:flex-row">
-            <div
-              onClick={() => setMethod("stripe")}
-              className="flex  items-center gap-3 border p-2 px-3 cursor-pointer"
-            >
-              <p
-                className={`min-w-3.5 h-3.5 border rounded-full  ${
-                  method === "stripe" ? "bg-green-400" : ""
-                }`}
-              ></p>
-              <img className="h-5 mx-4" src={assets.stripe_logo} alt="" />
-            </div>
+      <p className="text-center  text-slate-400 font-semibold  justify-center">
+        Email <span className="w-1 text-red-500 text-2xl font-bold">*</span>
+      </p>
+      <input
+      onChange={onChangeHandler} value={formData.email} name="email"
+        type="email"
+        className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+        placeholder="votre email"
+        required
+      />
+      <p className="text-center  text-slate-400 font-semibold  justify-center">
+        La rue <span className="w-1 text-red-500 text-2xl font-bold">*</span>
+      </p>
+      <input
+      onChange={onChangeHandler} name="rue" value={formData.rue}
+        type="text"
+        className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+        placeholder="La rue"
+        required
+      />
 
-           
-            <div
-              onClick={() => setMethod("cash")}
-              className="flex  items-center gap-3 border bg-slate-200 p-2 px-3 cursor-pointer"
-            >
-              <p
-                className={`min-w-3.5 h-3.5 border rounded-full  ${
-                  method === "cash" ? "bg-green-400" : ""
-                }`}
-              ></p>
-              <p className="text-gray-700 text-sm   uppercase font-medium mx-4">
-                Payé à la livraison
-              </p>
-            </div>
+      <p className="text-center  text-slate-400 font-semibold   justify-center">
+        Ville et la commune <span className="w-1 text-red-500 text-2xl font-bold">*</span>
+      </p>
+      <div className="flex gap-3">
+        <input
+        onChange={onChangeHandler}  value={formData.city}  name="city"
+          className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          type="text"
+          placeholder="Ville"
+          required
+        />
+        <input
+        onChange={onChangeHandler}  name="state" value={formData.state}
+          className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          type="text"
+          placeholder="Commune"
+          required
+        />
+      </div>
+
+      <p className="text-center  text-slate-400 font-semibold  justify-center">
+        Code postal et pays <span className="w-1 text-red-500 text-2xl font-bold">*</span>
+      </p>
+      <div className="flex gap-3">
+        <input
+        onChange={onChangeHandler} value={formData.zipcode}  name="zipcode"
+          className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          type="number"
+          placeholder="Code postal"
+          required
+        />
+        <input
+        onChange={onChangeHandler} name="country"  value={formData.country}
+          className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          type="text"
+          placeholder="Pays"
+          required
+        />
+      </div>
+
+      <p className="text-center  text-slate-400 font-semibold  justify-center">
+        {" "}
+        Votre numéro de téléphone portable  <span className="w-1 text-red-500 text-2xl font-bold">*</span>
+      </p>
+      <input
+      onChange={onChangeHandler} value={formData.phone} name="phone"
+        className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+        type="text"
+        placeholder="Votre numéro"
+        required
+      />
+     
+     <p className="text-center  text-slate-400 font-semibold  justify-center">
+        {" "}
+        Vous pouvez recevoir le colis sur un poit rélais  
+      </p>
+     
+      <input
+      onChange={onChangeHandler} value={formData.pointrelaix} name="pointrelaix"
+        className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+        type="text"
+        placeholder="Ecole, lieu plubic, boutique .. "
+      />
+    </div>
+    {/* 88888888888888 cotde droit ---- */}
+    <div className="mt-8">
+      <div className="mt-8 min-w-80">
+        <CartTotal />
+      </div>
+      <div className="mt-12">
+        <Title text1={"Methode de "} text2={"Payyement"} />
+        {/*Methode de  Pyament */}
+        <div className="flex gap-3 flex-col lg:flex-row">
+          <div
+            onClick={() => setMethod("stripe")}
+            className="flex  items-center gap-3 border p-2 px-3 cursor-pointer"
+          >
+            <p
+              className={`min-w-3.5 h-3.5 border rounded-full  ${
+                method === "stripe" ? "bg-green-400" : ""
+              }`}
+            ></p>
+            <img className="h-5 mx-4" src={assets.stripe_logo} alt="" />
           </div>
-          <div className=" w-full text-end mt-8">
-            <button
-              
-              className="bg-black text-white uppercase px-16 py-3 text-sm"
-            >
-              Valider votre commande
-            </button>
+
+         
+          <div
+            onClick={() => setMethod("cash")}
+            className="flex  items-center gap-3 border bg-slate-200 p-2 px-3 cursor-pointer"
+          >
+            <p
+              className={`min-w-3.5 h-3.5 border rounded-full  ${
+                method === "cash" ? "bg-green-400" : ""
+              }`}
+            ></p>
+            <p className="text-gray-700 text-sm   uppercase font-medium mx-4">
+              Payé à la livraison
+            </p>
           </div>
+        </div>
+        <div className=" w-full text-end mt-8">
+          <button
+            
+            className="bg-black text-white uppercase px-16 py-3 text-sm"
+          >
+            Valider votre commande
+          </button>
         </div>
       </div>
-    </form>
+    </div>
+  </form>
+       
   );
 };
 
